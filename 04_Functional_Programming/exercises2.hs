dffc091cd2be9192d6121ca7170b281d683798ec
James Carlson <jxxcarlson@mac.com>
2020-04-15 18:41:41 +0200

    update

--- 04_Functional_Programming/exercises2.hs
+++ 04_Functional_Programming/exercises2.hs
@@ -9,3 +9,12 @@ loop acc (x:xs) =
 
 asInt' xs = foldl reducer 0 xs
    where reducer acc digit = 10 * acc  + digitToInt digit
+
+
+
+asInt2 :: String -> Int
+asInt2 [] = error "Can't handle empty string"
+asInt2 "-" = error "Can't handle '-' unless digits following"
+asInt2 (x:xs)
+   | x == '-'       = -(asInt' xs)
+   | otherwise      = asInt' (x:xs)
---
+++ 05_JSON_library/README.md
@@ -0,0 +1,1 @@
+Correction: compile with 'ghc -o simple Main.hs', not as stated in the book
